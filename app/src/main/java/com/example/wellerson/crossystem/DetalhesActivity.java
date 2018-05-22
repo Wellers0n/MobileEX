9266dd9132782fd8f0c3b42ff56653018f867f57
Wellerson <Wellers0n_@hotmail.com>
2018-05-22 18:02:36 -0300

    Tirando os dois pontos no final do texto, só uma correção

--- app/src/main/java/com/example/wellerson/crossystem/DetalhesActivity.java
+++ app/src/main/java/com/example/wellerson/crossystem/DetalhesActivity.java
@@ -9,5 +9,9 @@ public class DetalhesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhes);
+
+
+
     }
+
 }
